d587451e7058cbca6204c4e17f9b45b5bb6ec8ab
Keith Kikta <kkikta@gmail.com>
2018-05-29 01:24:16 -0500

    Bump package version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oracle_drm_api",
-    version="0.1.0",
+    version="0.2.0",
     author="Keith Kikta",
     author_email="keith.kikta@tevpro.com",
     description="Oracle Data Relationship Management API Client",
